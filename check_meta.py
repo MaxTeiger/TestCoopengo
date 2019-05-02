e7e98ab74aee49a2800933b874cc2539db1af713
MaxTeiger <max.teiger@gmail.com>
2019-05-02 17:38:28 +0200

    A demain

--- check_meta.py
+++ check_meta.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 # vim: ft=python ts=4 sw=4 et
+# -*- encoding: utf-8 -*-
 
 import os
 import re
 import sys
 import requests
-import colors
+from colored import fg, bg, attr
+import base64
 
 # Retrieve from the environment variables usefull infos
 # after Drone as cloned the project
@@ -28,10 +30,13 @@ RM_HEADERS = {'X-Redmine-API-Key': RM_TOKEN}
 title_regexp = re.compile('\w+: .+')
 body_regexp = re.compile('.*(fix|ref) #(\d+)', re.M | re.I | re.S)
 changelog_regexp = re.compile('\* (BUG|FEA|OTH)#(\d+)')
+bug_regexp = re.compile('## \[title_en\]((.|\n)*)## \[title_fr\]((.|\n)*)### \[repro_fr\]((.|\n)*)### \[correction_fr\]((.|\n)*)\[parametrage_fr\]((.|\n)*)### \[scripts_fr\]((.|\n)*)## \[business_modules\]((.|\n)*)## \[original_description\]((.|\n)*$)')
+feature_regexp = re.compile('## \[title_en\]((.|\n)*)## \[title_fr\]((.|\n)*)### \[parametrage_fr\]((.|\n)*)### \[scripts_fr\]((.|\n)*)## \[business_modules\]((.|\n)*)## \[original_description\]((.|\n)*$)')
 
 # dict containing tracker name values
 rm_trackers = {1: 'bug', 2: 'fea'}
-issues_projects = [1, 31, 37]
+# 35 stand for Non-Coog
+issues_projects = [1, 31, 37, 35]
 
 # initialize variables
 gh_pull = None
@@ -40,7 +45,7 @@ gh_issue = None
 # list of labels for the specific pull request 
 gh_labels = None
 # list of modified files in the repository
-ghModifiedFiles = None
+ghIssueFiles = None
 # equals to the identifier defined in the body of the pull request 
 rm_issue = None
 
@@ -51,10 +56,10 @@ rm_issue_type = None
 # function to retrieve request and parse to JSON
 # in one time
 def requestInJson(url, headers):
-    r = requests.get(url, headers=GH_HEADERS)
+    r = requests.get(url, headers=headers)
 
     if r.status_code < 200 or r.status_code > 300:
-        print(('error:gh:{}:{}:{}'.format(url, r.status_code, r.text)))
+        print(('error   :gh:{}:{}:{}'.format(url, r.status_code, r.text)))
         sys.exit(1)
 
     return r.json()
@@ -83,13 +88,14 @@ def set_gh_labels():
 # Moreover, this function return the object from github
 def get_gh_files():
 
-    global ghModifiedFiles
-    ghModifiedFiles = dict()
+    global ghIssueFiles
+    ghIssueFiles = dict()
     url = (GH_URL_PULL + '/files').format(repo=REPO, pr=PR)
 
     gh_files = requestInJson(url, headers=GH_HEADERS)
     for f in gh_files:
-        ghModifiedFiles[f['filename']] = f['contents_url']
+        if str(rm_issue) +".md" in f['filename']:
+            ghIssueFiles[f['filename']] = f['contents_url']
     
     return gh_files
 
@@ -105,7 +111,7 @@ def check_labels():
         rm_issue_type = 'fea'
     elif 'bug' in gh_labels:
         rm_issue_type = 'bug'
-    print('labels:ok')
+    print(fg('green') +'labels  :ok' +attr(0))
     return True
 
 # check the title of the pull request (if it match the regex above
@@ -116,13 +122,13 @@ def check_labels():
 def check_title():
     ok = True
     if 'bypass title check' in gh_labels:
-        print('title:bypass')
+        print('title   :bypass')
     else:
         if title_regexp.match(gh_pull['title']):
-            print('title:ok')
+            print('title   :ok')
         else:
             ok = False
-            print('title:ko')
+            print('title   :ko')
     return ok
 
 # Check the body of the pull request
@@ -144,159 +150,98 @@ def check_body():
         # if both are defined but doesn't match 
         if issue and rm_issue and issue != rm_issue:
             ok = False
-            print(('body:ko:issue:{}-{}'.format(issue, rm_issue)))
+            print(('body    :ko:issue:{}-{}'.format(issue, rm_issue)))
         else:
             # set rm_issue
-            print(('body:ok:issue:{}'.format(issue)))
+            print(('body    :ok:issue:{}'.format(issue)))
             rm_issue = issue
 
     # if it doesn't match
     else:
         ok = False
-        print('body:ko')
+        print('body    :ko')
     return ok
 
+# Check if files are present and if the content match 
 
-def _check_content_changelog_line(label, line):
+# pattern if feature : ## \[title_en\]((.|\n)*)## \[title_fr\]((.|\n)*)### \[parametrage_fr\]((.|\n)*)### \[scripts_fr\]((.|\n)*)## \[business_modules\]((.|\n)*)## \[original_description\]((.|\n)*$)
+# grp1 = title_en, grp3 = title_fr, grp5 = parametrages_fr, grp7 = scripts_fr, grp9 = business modules, grp11 = original description
 
-    ok = True
-
-    # if there is a match
-    m = changelog_regexp.match(line)
-    if m:
-        issue_type = m.group(1).lower()
-        if issue_type != 'oth':
-            global rm_issue_type
-            if rm_issue_type and rm_issue_type != issue_type:
-                ok = False
-                print(('content:ko:changelog:{}:issue_type:{}{}'.format(
-                    label, rm_issue_type, issue_type)))
-            else:
-                rm_issue_type = issue_type
-                print(('content:ok:changelog:{}:issue_type:{}'.format(
-                    label, issue_type)))
-            issue = int(m.group(2))
-            global rm_issue
-            if rm_issue and rm_issue != issue:
-                ok = False
-                print(('content:ko:changelog:{}:issue:{}-{}'.format(
-                    label, issue, rm_issue)))
-            else:
-                rm_issue = issue
-                print(('content:ok:changelog:{}:issue:{}'.format(label, issue)))
-        else:
-            print(('content:ok:changelog:{}:issue_type:{}'.format(
-                label, issue_type)))
-    else:
-        ok = False
-        print(('content:ko:changelog:{}:{}'.format(label, line)))
-    return ok
-
-# Check the body of the pull request
-# if the body of the pull request match the
-# regex above,
-# define :
-# rm_issue
-# check run order : 3rd
-
+# pattern if bug : ## \[title_en\]((.|\n)*)## \[title_fr\]((.|\n)*)### \[repro_fr\]((.|\n)*)### \[correction_fr\]((.|\n)*)\[parametrage_fr\]((.|\n)*)### \[scripts_fr\]((.|\n)*)## \[business_modules\]((.|\n)*)## \[original_description\]((.|\n)*$)
+# grp1 = title_en, grp3 = title_fr, grp5 = Scenario de reproduction, grp7 = correction, grp9 = parametrages_fr, grp11 = scripts_fr, grp13 = business modules, grp15 = original description
 
 
 def check_content():
     ok = True
+    gh_files = get_gh_files()
 
-    # if the content check can be bypassed
-    if 'bypass content check' in gh_labels:
-        print('content:bypass')
+    for name, content in ghIssueFiles.items():
+        print("-------------------\nFilename :\t" +name +"\nContent URL :\t" +content +"\n\n----------------------------------------------------------------\n")
+        r = requestInJson(content, headers=GH_HEADERS)
+        fileContent = base64.b64decode(r['content'])
 
-    # otherwise
-    else:
-        # changelog list
-        changelogs = []
+    if real_issue_type == 'fea':
+        m = feature_regexp.match(fileContent.decode('utf8'))
+        title_en = m.group(1).replace("(required)","")
+        title_fr = m.group(3).replace("(required)","")
+        parametrage_fr = m.group(5).replace("<Paramétrage (éventuel) à faire>","")
+        scripts_fr = m.group(7).replace("<Scripts à passer>","")
+        business_modules = m.group(9)
+        original_description = m.group(11).replace("(required / automatic)","")
+        print(original_description)
+    
+    elif real_issue_type == 'bug':
+        
 
-        # for every files in the pull request
-        for f in get_gh_files():
-            # if the name of the file contains CHANGELOG
-            if 'CHANGELOG' in f['filename']:
-                # We add the file to the list
-                changelogs.append(f)
-        # if some filenames contain CHANGELOG
-        if changelogs:
-            # for each file
-            for changelog in changelogs:
-                # retrieve just the label of the changelog
-                label = changelog['filename'].split('/')[-2]
-                # retrieve the patch for the file
-                patch = changelog['patch']
-                lines = [
-                    line[1:].strip()
-                    for line in patch.splitlines()
-                    if line.startswith('+')]
-                if lines:
-                    if not _check_content_changelog_line(label, lines[0]):
-                        ok = False
-                else:
-                    ok = False
-                    print(('content:ko:changelog:{}'.format(
-                        changelog['filename'])))
-        else:
-            ok = False
-            print('content:ko:changelog')
     return ok
 
 
 #
 def check_redmine():
     ok = True
-    global rm_issue, rm_issue_type
+    global rm_issue, rm_issue_type, real_issue_type
 
     # if rm issue is defined
     if rm_issue:
         # connect to redmine
         url = RM_URL.format(issue=rm_issue)
-        r = requests.get(url, headers=RM_HEADERS)
+        issue = requestInJson(url, headers=RM_HEADERS)['issue']
 
-        # If an error is returned
-        if r.status_code < 200 or r.status_code > 300:
-            print(('error:rm:{}:{}:{}'.format(url, r.status_code, r.text)))
-            return False
-
-        # information about the specific issue
-        issue = r.json()['issue']
-        print(('redmine:ok:issue:{}'.format(issue['id'])))
+        print(('redmine :ok:issue:{}'.format(issue['id'])))
 
         # if issue type is defined (issue type == feature or bug)
         if rm_issue_type:
             # ensure the issue type is the same on redmine
             issue_type = rm_trackers[issue['tracker']['id']]
-
+            real_issue_type = issue_type
 
             # 
             if issue_type == rm_issue_type:
-                print(('redmine:ok:issue_type:{}'.format(issue_type)))
+                print(('redmine :ok:issue_type:{}'.format(issue_type)))
             else:
                 ok = False
-                print(('redmine:ko:issue_type:{}-{}'.format(
+                print(('redmine :ko:issue_type:{}-{}'.format(
                     issue_type, rm_issue_type)))
 
             issue_project = issue['project']['id']
             if issue_project in issues_projects:
-                print(('redmine:ok:issue_project:{}'.format(
+                print(('redmine :ok:issue_project:{}'.format(
                     issue['project']['name'])))
             else:
                 ok = False
-                print(('redmine:ko:issue_project:{}'.format(
+                print(('redmine :ko:issue_project:{}'.format(
                     issue['project']['name'])))
 
         # if issue type is not defined
         else:
             ok = False
-            print('redmine:ko:issue_type:empty')
+            print('redmine :ko:issue_type:empty')
     
     # if redmine issue is not defined
     elif rm_issue is None:
         # No problem if issue is #0000
         ok = False
-        print('redmine:ko:issue:empty')
+        print('redmine :ko:issue:empty')
 
     return ok
 
@@ -323,23 +268,13 @@ def main():
 
     # Special manner to check if every check is complete
     ok = True
-    ok = check_labels() and ok
-    ok = check_title() and ok
-    ok = check_body() and ok
-    ok = check_content() and ok
+    ok = check_labels()  and ok
+    ok = check_title()   and ok
+    ok = check_body()    and ok
     ok = check_redmine() and ok
+    ok = check_content() and ok
 
-    for name, content in ghModifiedFiles.items():
-        print("\n-------------\nFilename :\t" +name +"\nContent URL :\t" +content)
 
-    # # if checks are not validated
-    # if not ok:
-    #     # if meta-check can be bypassed
-    #     if 'bypass meta check' in gh_labels:
-    #         print('meta:bypass')
-    #     else:
-    #         # exit with an error
-    #         sys.exit(1)
 
     print("main:ok")
 
